e4a55461f03ab233967fbcfbe77c7a2878d65db0
Herman Singh <kartstig@gmail.com>
2015-04-18 14:32:13 -0400

    Images, JSON, shit

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,5 +1,6 @@
 class IndexController < ApplicationController
     def index
         @users = JSON.parse(File.read("users.json"))
+        @events = JSON.parse(File.read("events.json"))
     end
 end
