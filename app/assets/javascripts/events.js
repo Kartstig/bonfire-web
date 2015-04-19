7f9cb7421bcdb4e89abed925c106e92e8fbe6ee2
Herman Singh <kartstig@gmail.com>
2015-04-19 11:09:56 -0400

    UI stuff

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,4 +1,5 @@
 $(document).on("ready page:load", function() {
+
     $('.event-items').slick({
       infinite: true,
       slidesToShow: 5,
@@ -9,4 +10,13 @@ $(document).on("ready page:load", function() {
       slidesToShow: 3,
       slidesToScroll: 1
     });
+
+    $('.people-img').hover(
+      function () {
+        $(this).parent().find('p').fadeIn(150);
+      }, 
+      function () {
+        $(this).parent().find('p').fadeOut(150);
+      }
+    );  
 });
