6fed5ff89e241cd22b22b4747571ad2426eca198
Herman Singh <kartstig@gmail.com>
2015-04-19 00:31:47 -0400

    Gotta keep em seperated

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,7 +1,12 @@
 $(document).on("ready page:load", function() {
-    $('.people-items').slick({
+    $('.event-items').slick({
       infinite: true,
       slidesToShow: 5,
       slidesToScroll: 2
+    });
+    $('.people-items').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1
     });
 });
