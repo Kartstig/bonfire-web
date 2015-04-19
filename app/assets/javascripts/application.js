d596e98ca325ce4917ab3bec6cff3658bc9f4db9
Nicolas McCurdy <thenickperson@gmail.com>
2015-04-18 22:24:37 -0400

    Extract events.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,12 +15,3 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
-
-
-$(document).on("ready page:load", function() {
-    $('.people-items').slick({
-      infinite: true,
-      slidesToShow: 5,
-      slidesToScroll: 2
-    });
-});
---
+++ app/assets/javascripts/events.js
@@ -0,0 +1,7 @@
+$(document).on("ready page:load", function() {
+    $('.people-items').slick({
+      infinite: true,
+      slidesToShow: 5,
+      slidesToScroll: 2
+    });
+});
